472b9d3624c82bed3a20daf2837fcaf45dd9cb29
Noah Hammond <business.noah@hotmail.com>
2022-11-26 12:14:45 -0700

    commit tree 9c5699ebfc608bb727a094a2cf962ab49ad360b0

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+var bestCustomer
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'noah'
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'ty'
+}
